5f0a5d55f9a79f71f30b7e58bdcc2a38c147f6ae
adsonb87 <adsonb87@gmail.com>
2022-04-05 23:02:10 -0300

    Criacao do formulario de insercao e validacoes de campos

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidosRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidosRepository.java
@@ -1,9 +1,12 @@
 package br.com.alura.mvc.mudi.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 
-public interface PedidosRepository extends CrudRepository<Pedido, Long>{
+@Repository
+public interface PedidosRepository extends JpaRepository<Pedido, Long>{
 
+	
 }
